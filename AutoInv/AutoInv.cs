c7143fc709977bed88eef87dd38df1fd0509b565
Taidakun <nhimeis@gmail.com>
2024-08-25 17:05:11 +0200

    Added enable & disable commands

--- AutoInv/AutoInv.cs
+++ AutoInv/AutoInv.cs
@@ -9,6 +9,7 @@ namespace AutoInv
 {
     public class AutoInv : AOPluginEntry
     {
+        private bool enabled = true;
         [Obsolete]
         public override void Run(string pluginDir)
         {
@@ -16,7 +17,11 @@ namespace AutoInv
             {
                 Chat.WriteLine("AutoInv Plugin Loaded");
                 Chat.WriteLine("Any tell you receive with the word 'inv', 'invite' or 'team' will send the sender a team invite if there are room in your team and you've got leadership");
-
+                Chat.WriteLine("To disable auto-invites, use <font color='white'>/disableAutoInv</font>. To re-enable, use <font color='white'>/enableAutoInv</font>.");
+                Chat.WriteLine("Auto invite <font color='green'>enabled</font> !");
+                Chat.RegisterCommand("enableAutoInv", Command);
+                Chat.RegisterCommand("disableAutoInv", Command);
+                Chat.RegisterCommand("autoinv", Command);
                 Network.ChatMessageReceived += Network_ChatMessageReceived;
 
             }
@@ -26,8 +31,48 @@ namespace AutoInv
             }
         }
 
+        private void Command(string command, string[] param, ChatWindow chatWindow)
+        {
+            switch(command)
+            {
+                case "autoinv":
+                    if (enabled)
+                    {
+                        Chat.WriteLine("Current status: <font color='green'>enabled</font>.");
+                        Chat.WriteLine("To disable, use <font color='white'>/disableAutoInv</font>.");
+                    }
+                    else
+                    {
+                        Chat.WriteLine("Current status: <font color='red'>disabled</font>.");
+                        Chat.WriteLine("To enable, use <font color='white'>/enableAutoInv</font>.");
+                    }
+                    break;
+                case "enableAutoInv":
+                    if (enabled)
+                    {
+                        Chat.WriteLine("Already <font color='green'>enabled</font>, nothing happened.");
+                        return;
+                    }
+                    enabled = true;
+                    Chat.WriteLine("Auto invite <font color='green'>enabled</font> !");
+                    break;
+                case "disableAutoInv":
+                    if (!enabled)
+                    {
+                        Chat.WriteLine("Already <font color='red'>disabled</font>, nothing happened.");
+                        return;
+                    }
+                    enabled = false;
+                    Chat.WriteLine("Auto invite <font color='red'>disabled</font> !");
+                    break;
+                default:
+                    return;
+            }
+        }
+
         private void Network_ChatMessageReceived(object s, SmokeLounge.AOtomation.Messaging.Messages.ChatMessageBody chatMessage)
         {
+            if (!enabled) return;
             if (chatMessage.PacketType == ChatMessageType.PrivateMessage)
             {
                 switch(((PrivateMsgMessage)chatMessage).Text) {
