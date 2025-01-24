30c1c20c30ed5badf94f1227c9000300f9676244
Taidakun <nhimeis@gmail.com>
2025-01-24 14:51:25 +0100

    New packages

--- AutoInv/AutoInv.cs
+++ AutoInv/AutoInv.cs
@@ -1,29 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using AOSharp.Core;
 using AOSharp.Core.UI;
 using AOSharp.Common.GameData;
 using SmokeLounge.AOtomation.Messaging.Messages.ChatMessages;
 using SmokeLounge.AOtomation.Messaging.Messages;
+using SmokeLounge.AOtomation.Messaging.GameData;
 
 namespace AutoInv
 {
     public class AutoInv : AOPluginEntry
     {
         private bool enabled = true;
+        public string FILEBANNED;
+
+        HashSet<string> Banned = new HashSet<string>();
+        HashSet<BannedEntry> Banlist = new HashSet<BannedEntry>();
+        
         [Obsolete]
         public override void Run(string pluginDir)
         {
+            SimpleChar c = new SimpleChar(DynelManager.LocalPlayer);
+            // FILEBANNED = Path.Combine(Path.Combine(pluginDir, "Banlist"),  c.Name + " Banlist.txt");
             try
             {
+                // Checks banlist
+                string[] banList = File.ReadAllLines(FILEBANNED);
+                foreach (string banned in banList)
+                {
+                    Banned.Add(banned.ToUpper().Trim());
+                }
+
+                // Displays initialization messages
                 Chat.WriteLine("AutoInv Plugin Loaded");
                 Chat.WriteLine("Any tell you receive with the word 'inv', 'invite' or 'team' will send the sender a team invite if there are room in your team and you've got leadership");
                 Chat.WriteLine("To disable auto-invites, use <font color='white'>/disableAutoInv</font>. To re-enable, use <font color='white'>/enableAutoInv</font>.");
                 Chat.WriteLine("To see the current auto-invite status, use <font color='white'>/autoinv</font>.");
+
+                // Registers chat commands
                 Chat.RegisterCommand("enableAutoInv", Command);
                 Chat.RegisterCommand("disableAutoInv", Command);
                 Chat.RegisterCommand("autoinv", Command);
-                Network.ChatMessageReceived += Network_ChatMessageReceived;
+                Chat.RegisterCommand("temp", Command);
 
+                // Manages all tells
+                Network.ChatMessageReceived += Network_ChatMessageReceived;
             }
             catch (Exception e)
             {
@@ -65,27 +88,63 @@ namespace AutoInv
                     enabled = false;
                     Chat.WriteLine("Auto invite <font color='red'>disabled</font> !");
                     break;
+                case "temp":
+                    Chat.WriteLine(FILEBANNED);
+                    break;
                 default:
                     return;
             }
         }
 
-        private void Network_ChatMessageReceived(object s, SmokeLounge.AOtomation.Messaging.Messages.ChatMessageBody chatMessage)
+        private void Network_ChatMessageReceived(object s, ChatMessageBody chatMessage)
         {
             if (!enabled) return;
-            if (chatMessage.PacketType == ChatMessageType.PrivateMessage)
-            {
-                switch(((PrivateMsgMessage)chatMessage).Text) {
-                    case "invite":
-                    case "team":
-                    case "inv":
-                    Chat.WriteLine($"Received {((PrivateMsgMessage)chatMessage).Text} from {((PrivateMsgMessage)chatMessage).Sender}");
-                    Team.Invite(new Identity(IdentityType.SimpleChar, Convert.ToInt32(((PrivateMsgMessage)chatMessage).Sender)));
+            if (chatMessage.PacketType == ChatMessageType.PrivateMessage) ;
+            Identity player = new Identity(IdentityType.SimpleChar, Convert.ToInt32(((PrivateMsgMessage)chatMessage).Sender));
+            // SimpleChar temp = new SimpleChar(test);
+            switch(((PrivateMsgMessage)chatMessage).Text.ToLower()) {
+                case "invite":
+                case "team":
+                case "inv":
+                    Chat.WriteLine($"Received {((PrivateMsgMessage)chatMessage).Text} from {player}");
+                    Team.Invite(player);
                     break;
-                    default:
-                        break;
-                };
+                case "test":
+                    Chat.WriteLine("Test OK");
+                    break;
+                case "checkbanned":
+                    Chat.WriteLine("Checking Banned peeps...");
+                    string[] banList = File.ReadAllLines(FILEBANNED);
+                    if (banList.Length > 0)
+                    {
+                        Chat.WriteLine($"Currently {File.ReadAllLines(FILEBANNED).Length} people banned.");
+                        foreach (string banned in banList)
+                        {
+                            Chat.WriteLine(banned, ChatColor.White);
+                        }
+                    }
+                    else
+                        Chat.WriteLine("No one is banned for now !");
+                    break;
+                default:
+                    break;
+            };
+        }
+
+        public class BannedEntry
+        {
+            public string BannedName;
+            public string BannedReason;
+            public BannedEntry(string BannedName, string BannedReason)
+            {
+                this.BannedName = BannedName;
+                this.BannedReason = BannedReason;
             }
         }
+
+        //private bool isBanned(string User)
+        //{
+        //    return (Banned.Where(o => o == User.ToUpper()).FirstOrDefault() != null);
+        //}
     }
 }
